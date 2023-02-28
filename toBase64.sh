2dac137408c0d169abfd60e42c18f83e51cd1365
Moritz Löchner <momo.loechner@gmail.com>
2023-02-28 17:49:40 +0100

    wunderpass images

--- toBase64.sh
+++ toBase64.sh
@@ -1,7 +1,7 @@
-convert public/projects/coolhead/0.png -blur 50x50 -resize 4% public/projects/coolhead/0blur.jpg
-convert public/projects/coolhead/1.png -blur 50x50 -resize 4% public/projects/coolhead/1blur.jpg
-convert public/projects/coolhead/2.png -blur 50x50 -resize 4% public/projects/coolhead/2blur.jpg
-convert public/projects/coolhead/3.png -blur 50x50 -resize 4% public/projects/coolhead/3blur.jpg
-convert public/projects/coolhead/4.png -blur 50x50 -resize 4% public/projects/coolhead/4blur.jpg
+convert public/projects/wunderpass/0.png -blur 50x50 -resize 4% public/projects/wunderpass/0blur.jpg
+convert public/projects/wunderpass/1.png -blur 50x50 -resize 4% public/projects/wunderpass/1blur.jpg
+convert public/projects/wunderpass/2.png -blur 50x50 -resize 4% public/projects/wunderpass/2blur.jpg
+convert public/projects/wunderpass/3.png -blur 50x50 -resize 4% public/projects/wunderpass/3blur.jpg
+convert public/projects/wunderpass/4.png -blur 50x50 -resize 4% public/projects/wunderpass/4blur.jpg
 
-node toBase64.js public/projects/coolhead/0blur.jpg public/projects/coolhead/1blur.jpg public/projects/coolhead/2blur.jpg public/projects/coolhead/3blur.jpg public/projects/coolhead/4blur.jpg
+node toBase64.js public/projects/wunderpass/0blur.jpg public/projects/wunderpass/1blur.jpg public/projects/wunderpass/2blur.jpg public/projects/wunderpass/3blur.jpg public/projects/wunderpass/4blur.jpg
