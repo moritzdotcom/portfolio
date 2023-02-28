d15e1f123ed05cad3473f5e2b9f3784cb07fe021
Moritz Löchner <momo.loechner@gmail.com>
2023-02-28 19:46:27 +0100

    ERP Project

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import logo from '/public/logo.png';
 
 export default function Footer() {
   return (
-    <footer className="bg-gradient-to-r from-gray-900 via-zinc-900 to-indigo-900 text-white text-center shadow-xl">
+    <footer className="bg-gradient-to-r from-gray-900 via-zinc-900 to-indigo-900 text-white text-center shadow-xl mt-5">
       <div className="p-3 flex items-center justify-between max-w-screen-xl mx-auto">
         <Image src={logo} alt="ML Logo" width={30} />
         <p>© 2023 Moritz Löchner</p>
