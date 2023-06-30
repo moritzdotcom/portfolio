1591b715bef9d43bd5ef5b95abcc6dc1f72d4d3c
Moritz Löchner <momo.loechner@gmail.com>
2023-06-30 20:09:29 +0200

    Fix twilio api routes

--- pages/api/twilio/call.ts
+++ pages/api/twilio/call.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const file = fs.readFileSync('./public/twilio/call.xml', 'utf8');
+  const publicDir = path.join(process.cwd(), 'public');
+  const file = fs.readFileSync(publicDir + '/twilio/call.xml', 'utf8');
   res.setHeader('Content-Type', 'text/xml');
   res.status(200).send(file);
 }
--- pages/api/twilio/music.ts
+++ pages/api/twilio/music.ts
@@ -1,8 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import fs from 'fs';
+import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const file = fs.readFileSync('./public/twilio/keinproblem.mp3', 'utf8');
+  const publicDir = path.join(process.cwd(), 'public');
+  const file = fs.readFileSync(publicDir + '/twilio/keinproblem.mp3');
   res.setHeader('Content-Type', 'audio/mpeg');
   res.status(200).send(file);
 }
